29f94988ae9b7facb8cb90dcefbf5cb0c2724be0
Samuel Solis <ssolisfu@gmail.com>
2019-03-12 21:11:11 +0100

    Added the Model layer.

---
+++ Countries.php
@@ -0,0 +1,328 @@
+<?php
+
+/**
+ * Emulate a database with countries.
+ *
+ * Class Countries
+ */
+class Countries {
+
+  // array of countries $key => $value.
+  private $countries;
+
+  /**
+   * Countries constructor. Initialize the countries array.
+   */
+  public function __construct() {
+    $this->countries = [
+      'AF' => 'Afghanistan',
+      'AX' => 'Aland Islands',
+      'AL' => 'Albania',
+      'DZ' => 'Algeria',
+      'AS' => 'American Samoa',
+      'AD' => 'Andorra',
+      'AO' => 'Angola',
+      'AI' => 'Anguilla',
+      'AQ' => 'Antarctica',
+      'AG' => 'Antigua And Barbuda',
+      'AR' => 'Argentina',
+      'AM' => 'Armenia',
+      'AW' => 'Aruba',
+      'AU' => 'Australia',
+      'AT' => 'Austria',
+      'AZ' => 'Azerbaijan',
+      'BS' => 'Bahamas',
+      'BH' => 'Bahrain',
+      'BD' => 'Bangladesh',
+      'BB' => 'Barbados',
+      'BY' => 'Belarus',
+      'BE' => 'Belgium',
+      'BZ' => 'Belize',
+      'BJ' => 'Benin',
+      'BM' => 'Bermuda',
+      'BT' => 'Bhutan',
+      'BO' => 'Bolivia',
+      'BA' => 'Bosnia And Herzegovina',
+      'BW' => 'Botswana',
+      'BV' => 'Bouvet Island',
+      'BR' => 'Brazil',
+      'IO' => 'British Indian Ocean Territory',
+      'BN' => 'Brunei Darussalam',
+      'BG' => 'Bulgaria',
+      'BF' => 'Burkina Faso',
+      'BI' => 'Burundi',
+      'KH' => 'Cambodia',
+      'CM' => 'Cameroon',
+      'CA' => 'Canada',
+      'CV' => 'Cape Verde',
+      'KY' => 'Cayman Islands',
+      'CF' => 'Central African Republic',
+      'TD' => 'Chad',
+      'CL' => 'Chile',
+      'CN' => 'China',
+      'CX' => 'Christmas Island',
+      'CC' => 'Cocos (Keeling) Islands',
+      'CO' => 'Colombia',
+      'KM' => 'Comoros',
+      'CG' => 'Congo',
+      'CD' => 'Congo, Democratic Republic',
+      'CK' => 'Cook Islands',
+      'CR' => 'Costa Rica',
+      'CI' => 'Cote D\'Ivoire',
+      'HR' => 'Croatia',
+      'CU' => 'Cuba',
+      'CY' => 'Cyprus',
+      'CZ' => 'Czech Republic',
+      'DK' => 'Denmark',
+      'DJ' => 'Djibouti',
+      'DM' => 'Dominica',
+      'DO' => 'Dominican Republic',
+      'EC' => 'Ecuador',
+      'EG' => 'Egypt',
+      'SV' => 'El Salvador',
+      'GQ' => 'Equatorial Guinea',
+      'ER' => 'Eritrea',
+      'EE' => 'Estonia',
+      'ET' => 'Ethiopia',
+      'FK' => 'Falkland Islands (Malvinas)',
+      'FO' => 'Faroe Islands',
+      'FJ' => 'Fiji',
+      'FI' => 'Finland',
+      'FR' => 'France',
+      'GF' => 'French Guiana',
+      'PF' => 'French Polynesia',
+      'TF' => 'French Southern Territories',
+      'GA' => 'Gabon',
+      'GM' => 'Gambia',
+      'GE' => 'Georgia',
+      'DE' => 'Germany',
+      'GH' => 'Ghana',
+      'GI' => 'Gibraltar',
+      'GR' => 'Greece',
+      'GL' => 'Greenland',
+      'GD' => 'Grenada',
+      'GP' => 'Guadeloupe',
+      'GU' => 'Guam',
+      'GT' => 'Guatemala',
+      'GG' => 'Guernsey',
+      'GN' => 'Guinea',
+      'GW' => 'Guinea-Bissau',
+      'GY' => 'Guyana',
+      'HT' => 'Haiti',
+      'HM' => 'Heard Island & Mcdonald Islands',
+      'VA' => 'Holy See (Vatican City State)',
+      'HN' => 'Honduras',
+      'HK' => 'Hong Kong',
+      'HU' => 'Hungary',
+      'IS' => 'Iceland',
+      'IN' => 'India',
+      'ID' => 'Indonesia',
+      'IR' => 'Iran, Islamic Republic Of',
+      'IQ' => 'Iraq',
+      'IE' => 'Ireland',
+      'IM' => 'Isle Of Man',
+      'IL' => 'Israel',
+      'IT' => 'Italy',
+      'JM' => 'Jamaica',
+      'JP' => 'Japan',
+      'JE' => 'Jersey',
+      'JO' => 'Jordan',
+      'KZ' => 'Kazakhstan',
+      'KE' => 'Kenya',
+      'KI' => 'Kiribati',
+      'KR' => 'Korea',
+      'KW' => 'Kuwait',
+      'KG' => 'Kyrgyzstan',
+      'LA' => 'Lao People\'s Democratic Republic',
+      'LV' => 'Latvia',
+      'LB' => 'Lebanon',
+      'LS' => 'Lesotho',
+      'LR' => 'Liberia',
+      'LY' => 'Libyan Arab Jamahiriya',
+      'LI' => 'Liechtenstein',
+      'LT' => 'Lithuania',
+      'LU' => 'Luxembourg',
+      'MO' => 'Macao',
+      'MK' => 'Macedonia',
+      'MG' => 'Madagascar',
+      'MW' => 'Malawi',
+      'MY' => 'Malaysia',
+      'MV' => 'Maldives',
+      'ML' => 'Mali',
+      'MT' => 'Malta',
+      'MH' => 'Marshall Islands',
+      'MQ' => 'Martinique',
+      'MR' => 'Mauritania',
+      'MU' => 'Mauritius',
+      'YT' => 'Mayotte',
+      'MX' => 'Mexico',
+      'FM' => 'Micronesia, Federated States Of',
+      'MD' => 'Moldova',
+      'MC' => 'Monaco',
+      'MN' => 'Mongolia',
+      'ME' => 'Montenegro',
+      'MS' => 'Montserrat',
+      'MA' => 'Morocco',
+      'MZ' => 'Mozambique',
+      'MM' => 'Myanmar',
+      'NA' => 'Namibia',
+      'NR' => 'Nauru',
+      'NP' => 'Nepal',
+      'NL' => 'Netherlands',
+      'AN' => 'Netherlands Antilles',
+      'NC' => 'New Caledonia',
+      'NZ' => 'New Zealand',
+      'NI' => 'Nicaragua',
+      'NE' => 'Niger',
+      'NG' => 'Nigeria',
+      'NU' => 'Niue',
+      'NF' => 'Norfolk Island',
+      'MP' => 'Northern Mariana Islands',
+      'NO' => 'Norway',
+      'OM' => 'Oman',
+      'PK' => 'Pakistan',
+      'PW' => 'Palau',
+      'PS' => 'Palestinian Territory, Occupied',
+      'PA' => 'Panama',
+      'PG' => 'Papua New Guinea',
+      'PY' => 'Paraguay',
+      'PE' => 'Peru',
+      'PH' => 'Philippines',
+      'PN' => 'Pitcairn',
+      'PL' => 'Poland',
+      'PT' => 'Portugal',
+      'PR' => 'Puerto Rico',
+      'QA' => 'Qatar',
+      'RE' => 'Reunion',
+      'RO' => 'Romania',
+      'RU' => 'Russian Federation',
+      'RW' => 'Rwanda',
+      'BL' => 'Saint Barthelemy',
+      'SH' => 'Saint Helena',
+      'KN' => 'Saint Kitts And Nevis',
+      'LC' => 'Saint Lucia',
+      'MF' => 'Saint Martin',
+      'PM' => 'Saint Pierre And Miquelon',
+      'VC' => 'Saint Vincent And Grenadines',
+      'WS' => 'Samoa',
+      'SM' => 'San Marino',
+      'ST' => 'Sao Tome And Principe',
+      'SA' => 'Saudi Arabia',
+      'SN' => 'Senegal',
+      'RS' => 'Serbia',
+      'SC' => 'Seychelles',
+      'SL' => 'Sierra Leone',
+      'SG' => 'Singapore',
+      'SK' => 'Slovakia',
+      'SI' => 'Slovenia',
+      'SB' => 'Solomon Islands',
+      'SO' => 'Somalia',
+      'ZA' => 'South Africa',
+      'GS' => 'South Georgia And Sandwich Isl.',
+      'ES' => 'Spain',
+      'LK' => 'Sri Lanka',
+      'SD' => 'Sudan',
+      'SR' => 'Suriname',
+      'SJ' => 'Svalbard And Jan Mayen',
+      'SZ' => 'Swaziland',
+      'SE' => 'Sweden',
+      'CH' => 'Switzerland',
+      'SY' => 'Syrian Arab Republic',
+      'TW' => 'Taiwan',
+      'TJ' => 'Tajikistan',
+      'TZ' => 'Tanzania',
+      'TH' => 'Thailand',
+      'TL' => 'Timor-Leste',
+      'TG' => 'Togo',
+      'TK' => 'Tokelau',
+      'TO' => 'Tonga',
+      'TT' => 'Trinidad And Tobago',
+      'TN' => 'Tunisia',
+      'TR' => 'Turkey',
+      'TM' => 'Turkmenistan',
+      'TC' => 'Turks And Caicos Islands',
+      'TV' => 'Tuvalu',
+      'UG' => 'Uganda',
+      'UA' => 'Ukraine',
+      'AE' => 'United Arab Emirates',
+      'GB' => 'United Kingdom',
+      'US' => 'United States',
+      'UM' => 'United States Outlying Islands',
+      'UY' => 'Uruguay',
+      'UZ' => 'Uzbekistan',
+      'VU' => 'Vanuatu',
+      'VE' => 'Venezuela',
+      'VN' => 'Viet Nam',
+      'VG' => 'Virgin Islands, British',
+      'VI' => 'Virgin Islands, U.S.',
+      'WF' => 'Wallis And Futuna',
+      'EH' => 'Western Sahara',
+      'YE' => 'Yemen',
+      'ZM' => 'Zambia',
+      'ZW' => 'Zimbabwe',
+    ];
+  }
+
+  /**
+   * Search a country using the Country key.
+   *
+   * @param string $key
+   *    The country key.
+   * @return string
+   *   The country value, empty if doesn't exist.
+   */
+  public function searchByKey($key) {
+    return $this->countries[$key] ?: '';
+  }
+
+  /**
+   * Search a country using the country name.
+   *
+   * @param string $value
+   *   The country name.
+   *
+   * @return string
+   *   The country key, False in other case.
+   */
+  public function searchByValue($value) {
+    if ($key = array_search($value, $this->countries)) {
+      return $this->countries[$key] ?: '';
+    }
+
+    return FALSE;
+  }
+
+  /**
+   * Get the number of countries availables.
+   *
+   * @return int
+   */
+  public function count() {
+    return count($this->countries);
+  }
+
+  /**
+   * Get all countries in a key->values array.
+   * @return array
+   */
+  public function getAll() {
+    return $this->countries;
+  }
+
+  /**
+   * Filter the country list using a string.
+   *
+   * @param string $search
+   *   The string to filter.
+   * @return array
+   *   The countries that match with $search.
+   */
+  public function filter($search) {
+    return array_filter($this->countries, function($value) use ($search) {
+      if (strpos($value, $search) !== FALSE) {
+        return TRUE;
+      }
+      return FALSE;
+    });
+  }
+}
--- index.php
+++ index.php
@@ -2,17 +2,13 @@
 
 require_once("vendor/autoload.php");
 require_once ("Car.php");
+require_once ("Countries.php");
 
 $loader = new \Twig_Loader_Filesystem(__DIR__.'/templates');
 $twig = new \Twig_Environment($loader);
 
-$car1 = new Car('Samuel');
-$car1->refuel();
-$car1->speedUp(20);
-echo $twig->render('car.twig', ['car' => $car1]);
-
-$car2 = new Car('Beatriz');
-$car2->refuel();
-$car2->speedUp(40);
-$car2->speedDown(5);
-echo $twig->render('car.twig', ['car' => $car2]);
+$database_countries = new Countries();
+var_dump($database_countries->count());
+var_dump($database_countries->searchByKey('ES'));
+var_dump($database_countries->searchByValue('Spain'));
+var_dump($database_countries->filter('An'));
