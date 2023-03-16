d379a0a4ec35a41d675b84b7d24c8a41134b2aaf
Samuel Solís <samuel.solis@gruposantander.com>
2023-03-16 17:32:15 +0100

    Adapted code to Slim 4.

--- index.php
+++ index.php
@@ -1,17 +1,53 @@
 <?php
-require_once("vendor/autoload.php");
+
+require __DIR__ . '/vendor/autoload.php';
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
 
 require_once ("Database.php");
 require_once ("Color.php");
 require_once ("ColorForm.php");
 require_once ("ColorList.php");
 
-$app = new Slim\App();
+/**
+ * Instantiate App
+ *
+ * In order for the factory to work you need to ensure you have installed
+ * a supported PSR-7 implementation of your choice e.g.: Slim PSR-7 and a supported
+ * ServerRequest creator (included with Slim PSR-7)
+ */
+$app = AppFactory::create();
+
+/**
+ * The routing middleware should be added earlier than the ErrorMiddleware
+ * Otherwise exceptions thrown from it will not be handled by the middleware
+ */
+$app->addRoutingMiddleware();
+
+/**
+ * Add Error Middleware
+ *
+ * @param bool                  $displayErrorDetails -> Should be set to false in production
+ * @param bool                  $logErrors -> Parameter is passed to the default ErrorHandler
+ * @param bool                  $logErrorDetails -> Display error details in error log
+ * @param LoggerInterface|null  $logger -> Optional PSR-3 Logger
+ *
+ * Note: This middleware should be added last. It will not handle any exceptions/errors
+ * for middleware added after it.
+ */
+$errorMiddleware = $app->addErrorMiddleware(true, true, true);
+
+$app->get('/', function (Request $request, Response $response, $args) {
+    $response->getBody()->write("Go to /colors for get a list of colors!");
+    return $response;
+});
 
 /**
  * Return a simple color list.
  */
-$app->get("/colors", function ($request) {
+$app->get("/colors", function (Request $request, Response $response,array $arguments) {
   $colorList = new ColorList();
   $colores = $colorList->getAll();
   $data = [];
@@ -24,13 +60,18 @@ $app->get("/colors", function ($request) {
       'price' => $color->getPrice(),
     ];
   }
-  echo json_encode($data);
+
+  // Configure the header as a Json content.
+  header('Content-Type: application/json');
+  $response->getBody()->write(json_encode($data));
+
+  return $response;
 });
 
 /**
  * Create a new color.
  */
-$app->post("/color", function ($request, $response, $arguments) {
+$app->post("/color", function (Request $request, Response $response, array $arguments) {
 
   $database = new Database();
   $params = $request->getParsedBody();
@@ -41,16 +82,21 @@ $app->post("/color", function ($request, $response, $arguments) {
   $color->setPrice($params['price']);
   $color->save();
 
+  header('Content-Type: application/json');
+  $response->getBody()->write(json_encode($color));
+
+  return $response;
 });
 
 /**
  * Edit a color.
  */
-$app->patch("/color/{hex}", function ($request, $response, $arguments) {
+$app->patch("/color/{hex}", function (Request $request,Response $response, array $arguments) {
 
   $database = new Database();
+  $hex = $arguments['hex'];
   $params = $request->getParsedBody();
-  $hex = $request->getAttribute('hex');
+
 
   $color = new Color($database);
   $color->load($hex);
@@ -58,18 +104,23 @@ $app->patch("/color/{hex}", function ($request, $response, $arguments) {
   $color->setPrice($params['price']);
   $color->save();
 
+    header('Content-Type: application/json');
+    $response->getBody()->write(json_encode($color));
+
+    return $response;
 });
 
 /**
  * Delete a color from the database.
  */
-$app->delete("/color/{hex}", function ($request, $response, $arguments) {
+$app->delete("/color/{hex}", function (Request $request, Response $response, array $arguments) {
   $database = new Database();
-  $params = $request->getParsedBody();
-  $hex = $request->getAttribute('hex');
+  $hex = $arguments['hex'];
   $color = new Color($database);
   $color->load($hex);
   $color->delete();
+
+  return $response;
 });
 
 $app->run();
