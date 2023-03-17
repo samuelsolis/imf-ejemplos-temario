8136a870d474cf526baabc4607e4f3cd6c5d8c0a
Samuel Solís <samuel.solis@gruposantander.com>
2023-03-17 13:48:06 +0100

    Updated to php 8.0 version

--- index.php
+++ index.php
@@ -2,13 +2,19 @@
 
 require 'vendor/autoload.php';
 
-$loader = new \Twig_Loader_Filesystem(__DIR__.'/templates');
-$twig = new \Twig_Environment($loader, ['debug' => true]);
+$loader = new \Twig\Loader\FilesystemLoader(__DIR__.'/templates');
+$twig = new \Twig\Environment($loader);
 $twig->addExtension(new \Twig\Extension\DebugExtension());
 
 
 $client = new GuzzleHttp\Client();
-$res = $client->request('GET', 'https://datos.madrid.es/egob/catalogo/206717-0-agenda-eventos-bibliotecas.json', []);
+try {
+    $res = $client->request('GET', 'https://datos.madrid.es/egob/catalogo/206717-0-agenda-eventos-bibliotecas.json', []);
+}
+catch (\GuzzleHttp\Exception\GuzzleException $e) {
+    print '<p>Something was wrong in the server</p>';
+    exit;
+}
 
 
 switch ($res->getStatusCode()) {
